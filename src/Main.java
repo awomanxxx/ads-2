4c8531c33fe66cec9d37760d8b7dc461f2cc116b
awomanxxx <alimaqqb@gmail.com>
2026-04-11 00:59:20 +0500

    mplement yrrayist

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args) {
+        MyArrayList<Integer> list = new MyArrayList<>();
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+        list.add(10);
+        list.add(20);
+        list.addFirst(5);
+        list.addLast(30);
+
+        System.out.println(list.get(0));
+        System.out.println(list.getFirst());
+        System.out.println(list.getLast());
+        System.out.println(list.size());
+
+        list.remove(1);
+        System.out.println(list.size());
+
+        list.sort();
+
+        for (Integer x : list) {
+            System.out.println(x);
+        }
     }
 }
--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,2 +1,200 @@
-public class MyArrayList {
+import java.util.Iterator;
+
+public class MyArrayList<T extends Comparable<T>> implements MyList<T> {
+    private Object[] elements;
+    private int size;
+
+    public MyArrayList() {
+        elements = new Object[10];
+        size = 0;
+    }
+
+    private void ensureCapacity() {
+        if (size == elements.length) {
+            Object[] newArray = new Object[elements.length * 2];
+            for (int i = 0; i < size; i++) {
+                newArray[i] = elements[i];
+            }
+            elements = newArray;
+        }
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index);
+        }
+    }
+
+    private void checkIndexForAdd(int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index: " + index);
+        }
+    }
+
+    @Override
+    public void add(T item) {
+        ensureCapacity();
+        elements[size] = item;
+        size++;
+    }
+
+    @Override
+    public void set(int index, T item) {
+        checkIndex(index);
+        elements[index] = item;
+    }
+
+    @Override
+    public void add(int index, T item) {
+        checkIndexForAdd(index);
+        ensureCapacity();
+
+        for (int i = size; i > index; i--) {
+            elements[i] = elements[i - 1];
+        }
+
+        elements[index] = item;
+        size++;
+    }
+
+    @Override
+    public void addFirst(T item) {
+        add(0, item);
+    }
+
+    @Override
+    public void addLast(T item) {
+        add(item);
+    }
+
+    @Override
+    public T get(int index) {
+        checkIndex(index);
+        return (T) elements[index];
+    }
+
+    @Override
+    public T getFirst() {
+        if (size == 0) {
+            throw new RuntimeException("List is empty");
+        }
+        return (T) elements[0];
+    }
+
+    @Override
+    public T getLast() {
+        if (size == 0) {
+            throw new RuntimeException("List is empty");
+        }
+        return (T) elements[size - 1];
+    }
+
+    @Override
+    public void remove(int index) {
+        checkIndex(index);
+
+        for (int i = index; i < size - 1; i++) {
+            elements[i] = elements[i + 1];
+        }
+
+        elements[size - 1] = null;
+        size--;
+    }
+
+    @Override
+    public void removeFirst() {
+        if (size == 0) {
+            throw new RuntimeException("List is empty");
+        }
+        remove(0);
+    }
+
+    @Override
+    public void removeLast() {
+        if (size == 0) {
+            throw new RuntimeException("List is empty");
+        }
+        elements[size - 1] = null;
+        size--;
+    }
+
+    @Override
+    public void sort() {
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                T current = (T) elements[j];
+                T next = (T) elements[j + 1];
+
+                if (current.compareTo(next) > 0) {
+                    Object temp = elements[j];
+                    elements[j] = elements[j + 1];
+                    elements[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    @Override
+    public int indexOf(Object object) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(object)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object object) {
+        for (int i = size - 1; i >= 0; i--) {
+            if (elements[i].equals(object)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public boolean exists(Object object) {
+        return indexOf(object) != -1;
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] result = new Object[size];
+        for (int i = 0; i < size; i++) {
+            result[i] = elements[i];
+        }
+        return result;
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            elements[i] = null;
+        }
+        size = 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            int current = 0;
+
+            @Override
+            public boolean hasNext() {
+                return current < size;
+            }
+
+            @Override
+            public T next() {
+                return (T) elements[current++];
+            }
+        };
+    }
 }
